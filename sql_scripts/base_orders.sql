aa18611e31e6f5f7d0cbf9b89a660bdbab010538
Gaurav Tawri <gauravtawri001@gmail.com>
2025-04-13 19:40:07 +0530

    Base view that joins customer details with orders. Used as a foundation for further enrichment in the final view.

--- sql_scripts/base_orders.sql
+++ sql_scripts/base_orders.sql
@@ -1,1 +1,15 @@
+CREATE OR REPLACE VIEW base_orders AS
+SELECT o.order_id,
+       o.purchase_date,
+       o.approved_date,
+       o.delivered_carrier_date,
+       o.delivered_customer_date,
+       o.estimated_delivery_date,
+       c.customer_id,
+       c.customer_zip_code_prefix,
+       c.customer_city,
+       c.customer_state
+FROM orders o
+LEFT JOIN customers c ON o.customer_id = c.customer_id;
+
 select* from base_orders 
